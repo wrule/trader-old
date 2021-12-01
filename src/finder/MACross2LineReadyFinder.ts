97e62e9ee36d7870837f6268817f72fbdca9a263
jimao <jimao@jimaodeMacBook-Pro.local>
2021-12-01 18:23:37 +0800

    finder

--- src/finder/MACross2LineReadyFinder.ts
+++ src/finder/MACross2LineReadyFinder.ts
@@ -1,5 +1,14 @@
+import { IDayData } from '../dayData';
 
 export
 class MACross2LineReadyFinder {
+  public constructor(
+    dataSet: IDayData[][],
+    start: number,
+    end: number,
+  ) { }
 
+  public Find() {
+    
+  }
 }

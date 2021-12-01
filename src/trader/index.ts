2bdc7a329958e905ce74a0c9f98fce2383d933fc
jimao <jimao@jimaodeMacBook-Pro.local>
2021-12-01 16:27:11 +0800

    简化trader

--- src/trader/index.ts
+++ src/trader/index.ts
@@ -1,4 +1,5 @@
 import { IDayData } from '../dayData';
+import { ITradeData, TradeLog } from './log';
 
 export
 class Trader {
@@ -10,20 +11,20 @@ class Trader {
 
   private assets = 0;
 
-  public get Funds() {
-    return this.funds;
-  }
-
-  public get Assets() {
-    return this.assets;
-  }
-
   public get Holding() {
     return this.assets > 0;
   }
 
+  private data: ITradeData[] = [];
+  private log: TradeLog[] = [];
+
+  public get Log() {
+    return this.log;
+  }
+
   public Buy(data: IDayData) {
     if (!this.Holding) {
+      this.data = [{ ...data, funds: this.funds }];
       this.assets = this.funds / data.price * this.buyFee;
       this.funds = 0;
     }
@@ -33,6 +34,8 @@ class Trader {
     if (this.Holding) {
       this.funds = this.assets * data.price * this.sellFee;
       this.assets = 0;
+      this.data.push({ ...data, funds: this.funds });
+      this.log.push(new TradeLog(this.data));
     }
   }
 }
--- src/trader/log.ts
+++ src/trader/log.ts
@@ -1,5 +1,18 @@
+import { IDayData } from '../dayData';
 
 export
-interface ILog {
+interface ITradeData
+extends IDayData {
+  funds: number;
+}
 
+export
+class TradeLog {
+  public constructor(
+    private data: ITradeData[],
+  ) { }
+
+  public get Income() {
+    return (this.data[1].funds - this.data[0].funds) / this.data[0].funds;
+  }
 }

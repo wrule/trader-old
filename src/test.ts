16d948b3d30534a8ec346f20cd71152e8c975343
jimao <jimao@jimaodeMacBook-Pro.local>
2021-12-01 20:08:34 +0800

    提交

--- src/test.ts
+++ src/test.ts
@@ -3,28 +3,39 @@ import { Cross2Ready } from './strategy/Cross2Ready';
 import data from './data/btc.json';
 import { loadFromCoinmarketcapData } from './data';
 import { nums } from '@wrule/nums';
+import { MACross2LineReadyFinder } from './finder/MACross2LineReadyFinder';
 
 const dayData = loadFromCoinmarketcapData(data);
 const dayPrice = nums(dayData.map((item) => item.price));
 
-const strategy = new Cross2Ready(
-  dayPrice.MA(8),
-  dayPrice.MA(44),
-);
-const trader = new Trader(strategy);
-trader.Backtesting(dayData);
+let dataSet = Array(10)
+  .fill(0)
+  .map(() => dayPrice.randomRangeEndStart(365))
+  .map(([end, start]) => dayData.slice(start, end));
 
-console.log(
-  trader.winNums.min(),
-  trader.winNums.avg(),
-  trader.winNums.max(),
-  trader.winNums.standardDeviation(),
-);
-console.log(
-  trader.loseNums.max(),
-  trader.loseNums.avg(),
-  trader.loseNums.min(),
-  trader.loseNums.standardDeviation(),
-);
-console.log(trader.winRate, trader.income, trader.Log.length);
-console.log(trader.winNums);
+const finder = new MACross2LineReadyFinder(dataSet, 1, 120);
+const map = finder.Find();
+console.log(map.size);
+// console.log(dataSet);
+
+// const strategy = new Cross2Ready(
+//   dayPrice.MA(8),
+//   dayPrice.MA(44),
+// );
+// const trader = new Trader(strategy);
+// trader.Backtesting(dayData);
+
+// console.log(
+//   trader.winNums.min(),
+//   trader.winNums.avg(),
+//   trader.winNums.max(),
+//   trader.winNums.standardDeviation(),
+// );
+// console.log(
+//   trader.loseNums.max(),
+//   trader.loseNums.avg(),
+//   trader.loseNums.min(),
+//   trader.loseNums.standardDeviation(),
+// );
+// console.log(trader.winRate, trader.income, trader.Log.length);
+// console.log(trader.winNums);

7da0dac9323eb35e1a04fbe58c7a11383168e269
jimao <jimao@jimaodeMacBook-Pro.local>
2021-12-02 19:47:35 +0800

    持续亏损集

--- src/trader/tradeList.ts
+++ src/trader/tradeList.ts
@@ -3,7 +3,7 @@ import { Trade } from './trade';
 import moment from 'moment';
 
 export
-class TradeList {
+class Trades {
   public constructor(
     private trades: Trade[],    
   ) { }
@@ -21,11 +21,11 @@ class TradeList {
   }
 
   public profitTrades() {
-    return new TradeList(this.trades.filter((trade) => trade.Income > 0));
+    return new Trades(this.trades.filter((trade) => trade.Income > 0));
   }
 
   public lossTrades() {
-    return new TradeList(this.trades.filter((trade) => trade.Income <= 0));
+    return new Trades(this.trades.filter((trade) => trade.Income <= 0));
   }
 
   public profitRate() {
@@ -36,13 +36,44 @@ class TradeList {
     return this.lossTrades.length / this.length;
   }
 
+  public lossTradeSet() {
+    let start = -1;
+    const indexs: [number, number][] = [];
+    this.trades.forEach((trade, index) => {
+      const prevTrade = this.trades[index - 1] || null;
+      if (
+        trade.Income <= 0 &&
+        (prevTrade == null || prevTrade.Income > 0)
+      ) {
+        start = index;
+      }
+      if (start >= 0) {
+        if (trade.Income > 0) {
+          indexs.push([index, start]);
+          start = -1;
+        } else if (index >= this.trades.length - 1) {
+          indexs.push([index + 1, start]);
+          start = -1;
+        }
+      }
+    });
+    indexs.sort((a, b) => (b[0] - b[1]) - (a[0] - a[1]));
+    return indexs.map(
+      ([end, start]) => new Trades(this.trades.slice(start, end))
+    );
+  }
+
+  public profitTradeSet() {
+
+  }
+
   public select(
     start?: string,
     end?: string,
   ) {
     const startTime = start != null ? moment(start).valueOf() : -Infinity;
     const endTime = end != null ? moment(end).valueOf() : Infinity;
-    return new TradeList(
+    return new Trades(
       this.trades.filter(
         (trade) => (
           trade.BuyData.time >= startTime &&

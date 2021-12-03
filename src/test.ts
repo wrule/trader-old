221354c87508ff3e5037d0020055f95a10aaead5
jimao <jimao@jimaodeMacBook-Pro.local>
2021-12-03 18:31:29 +0800

    eth数据

--- src/test.ts
+++ src/test.ts
@@ -1,12 +1,12 @@
 import { Trader } from './trader';
 import { Cross2Ready } from './strategy/Cross2Ready';
-import data from './data/btc.json';
+import data from './data/eth.json';
 import { loadFromCoinmarketcapData } from './data';
 import { nums } from '@wrule/nums';
 
 const dayData = loadFromCoinmarketcapData(data);
 const dayPrice = nums(dayData.map((item) => item.price));
-const strategy = new Cross2Ready(dayPrice.MA(8), dayPrice.MA(44));
+const strategy = new Cross2Ready(dayPrice.MA(12), dayPrice.MA(21));
 const trader = new Trader(strategy);
 trader.Backtesting(dayData);
 

80cb019ad1e2322c95eae1f6c1c8015e71599bfe
jimao <jimao@jimaodeMacBook-Pro.local>
2021-12-03 14:53:42 +0800

    加上注释

--- src/trader/trade.ts
+++ src/trader/trade.ts
@@ -1,26 +1,44 @@
 import { IDayData } from '../dayData';
 
+/**
+ * 交易数据
+ */
 export
 interface ITradeData
 extends IDayData {
+  /**
+   * 交易涉及资金
+   */
   funds: number;
 }
 
+/**
+ * 交易
+ */
 export
 class Trade {
   public constructor(
-    private data: ITradeData[],
+    private data: [ITradeData, ITradeData],
   ) { }
 
+  /**
+   * 买入交易数据
+   */
   public get buyData() {
     return this.data[0];
   }
 
+  /**
+   * 出售交易数据
+   */
   public get sellData() {
     return this.data[1];
   }
 
+  /**
+   * 收益
+   */
   public get income() {
-    return (this.data[1].funds - this.data[0].funds) / this.data[0].funds;
+    return (this.sellData.funds - this.buyData.funds) / this.buyData.funds;
   }
 }

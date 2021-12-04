eae9cd8609f5c09e4f146e7df5ac990b0b80d6bf
jimao <jimao@jimaodeMBP.lan>
2021-12-04 17:20:44 +0800

    初步报告

--- src/trader/report.ts
+++ src/trader/report.ts
@@ -10,11 +10,23 @@ function pnum(num: number) {
 
 export function printReport(trader: Trader) {
 
-  console.log('[总盈利%]:', pnum(trader.TradeList.TotalIncome));
-  console.log('[交易次数]:', trader.TradeList.Length);
   console.log(
+    '[总盈利%]:', pnum(trader.TradeList.TotalIncome),
     '[胜率%]:', pnum(trader.TradeList.ProfitRate),
+  );
+  console.log(
+    '[交易次数]:', trader.TradeList.Length,
     '[盈利次数]:', trader.TradeList.ProfitTrades.Length,
     '[亏损次数]:', trader.TradeList.LossTrades.Length,
+  );
+  console.log(
+    '[单次最小盈利%]:', pnum(trader.TradeList.ProfitTrades.Incomes.min()),
+    '[单次平均盈利%]:', pnum(trader.TradeList.ProfitTrades.Incomes.avg()),
+    '[单次最大盈利%]:', pnum(trader.TradeList.ProfitTrades.Incomes.max()),
+  );
+  console.log(
+    '[单次最小亏损%]:', pnum(trader.TradeList.LossTrades.Incomes.max()),
+    '[单次平均亏损%]:', pnum(trader.TradeList.LossTrades.Incomes.avg()),
+    '[单次最大亏损%]:', pnum(trader.TradeList.LossTrades.Incomes.min()),
   );
 }

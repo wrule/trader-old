8a64c2e7bca67980b8a93b68b35e0314fab775a5
jimao <jimao@jimaodeMacBook-Pro.local>
2021-12-06 16:07:44 +0800

    fix: 连续亏损

---
+++ src/strategy/Cross3LineReady.ts
@@ -0,0 +1,36 @@
+import { Nums } from '@wrule/nums';
+import { Strategy } from '.';
+
+/**
+ * 三线穿越策略
+ */
+export
+class Cross3LineReady
+extends Strategy {
+  public constructor(
+    private fast: Nums,
+    private slow: Nums,
+    private guide: Nums,
+  ) {
+    super();
+  }
+
+  public Work() {
+    if (this.index < 1) {
+      return false;
+    }
+    if (
+      this.fast.Nums[this.indexBefore()] <= this.slow.Nums[this.indexBefore()] &&
+      this.fast.Nums[this.index] > this.slow.Nums[this.index]
+    ) {
+      this.trader.Buy(this.data);
+    }
+    if (
+      this.fast.Nums[this.indexBefore()] >= this.slow.Nums[this.indexBefore()] &&
+      this.fast.Nums[this.index] < this.slow.Nums[this.index]
+    ) {
+      this.trader.Sell(this.data);
+    }
+    return true;
+  }
+}

f10182156c43e2a1e893eed8952db8c5686aa46e
jimao <jimao@jimaodeMacBook-Pro.local>
2021-12-07 17:37:22 +0800

    eth实验

--- src/test.ts
+++ src/test.ts
@@ -1,23 +1,19 @@
 import { Trader } from './trader';
-import { Cross2Ready } from './strategy/Cross2Ready';
-import data from './data/btc.json';
+import data from './data/eth.json';
 import { loadFromCoinmarketcapData } from './data';
 import { nums } from '@wrule/nums';
-import { printReport } from './trader/report';
-import { MACross2LineReadyFinder } from './finder/MACross2LineReadyFinder';
-import fs from 'fs';
 import { Cross2LineReady } from './strategy/Cross2LineReady';
 import moment from 'moment';
-import { Cross3LineReady } from './strategy/Cross3LineReady';
 
 
 const dayData = loadFromCoinmarketcapData(data);
 const dayPrice = nums(dayData.map((item) => item.price));
-const strategy = new Cross2LineReady(dayPrice.MA(8), dayPrice.MA(44));
+const strategy = new Cross2LineReady(dayPrice.MA(12), dayPrice.MA(21));
 const trader = new Trader(strategy);
 trader.Backtesting(dayData);
-console.log(trader.TradeList.TotalIncome, trader.TradeList.TotalAppreciation);
-trader.TradeList.PrintReport();
+// trader.TradeList.PrintReport();
+
+trader.TradeList.Select('2017-01').PrintReport();
 
 const a = [
   moment('2013-07-05'),
@@ -29,13 +25,3 @@ const a = [
   moment('2020-03-12'),
   moment('2021-11-08'),
 ];
-
-a.forEach((item, index) => {
-  if (index < a.length - 1) {
-    console.log(item.format('YYYY-MM-DD'), index % 2 ? '顶' : '底');
-    const next = a[index + 1];
-    console.log((next.unix() - item.unix()) / 60 / 60 / 24);
-  } else {
-    console.log(item.format('YYYY-MM-DD'), index % 2 ? '顶' : '底');
-  }
-});

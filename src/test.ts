724898211a6dcfbffe883fe6107d2751195ff0c9
jimao <jimao@jimaodeMacBook-Pro.local>
2021-12-08 12:35:08 +0800

    fix: 10 25

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,5 @@
 import { Trader } from './trader';
-import data from './data/eth.json';
+import data from './data/dent.json';
 import { loadFromCoinmarketcapData } from './data';
 import { nums } from '@wrule/nums';
 import { Cross2LineReady } from './strategy/Cross2LineReady';
@@ -8,20 +8,9 @@ import moment from 'moment';
 
 const dayData = loadFromCoinmarketcapData(data);
 const dayPrice = nums(dayData.map((item) => item.price));
-const strategy = new Cross2LineReady(dayPrice.MA(12), dayPrice.MA(21));
+const strategy = new Cross2LineReady(dayPrice.MA(10), dayPrice.MA(25));
 const trader = new Trader(strategy);
 trader.Backtesting(dayData);
-// trader.TradeList.PrintReport();
+trader.TradeList.PrintReport();
 
-trader.TradeList.Select('2017-01').PrintReport();
-
-const a = [
-  moment('2013-07-05'),
-  moment('2013-12-04'),
-  moment('2015-01-14'),
-  moment('2017-12-16'),
-  moment('2018-12-15'),
-  moment('2019-06-26'),
-  moment('2020-03-12'),
-  moment('2021-11-08'),
-];
+// trader.TradeList.Select('2017-01').PrintReport();

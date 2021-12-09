5627cbd74277e89b1d884aa72a0df9d80d083095
jimao <jimao@jimaodeMacBook-Pro.local>
2021-12-09 12:45:17 +0800

    eth

--- src/test.ts
+++ src/test.ts
@@ -1,10 +1,11 @@
 import { Trader } from './trader';
-import data from './data/fil.json';
+import data from './data/eth.json';
 import { loadFromCoinmarketcapData } from './data';
 import { nums } from '@wrule/nums';
 import { Cross2LineReady } from './strategy/Cross2LineReady';
 import moment from 'moment';
 import { MACross2LineReadyFinder } from './finder/MACross2LineReadyFinder';
+import fs from 'fs';
 
 const dayData = loadFromCoinmarketcapData(data);
 const finder = new MACross2LineReadyFinder([dayData], 1, 120);
@@ -15,18 +16,22 @@ const list = Array.from(result.entries()).map(([key, value]) => {
   return { key, trader };
 });
 list.sort((a, b) => b.trader.TradeList.TotalIncome - a.trader.TradeList.TotalIncome);
-console.log(list.slice(0, 100).map((item) => [
+const a = list.slice(0, 100).map((item) => [
   item.key,
   item.trader.TradeList.TotalIncome,
   item.trader.TradeList.ProfitRate,
-  item.trader.TradeList.Select('2013-08', '2015-05').TotalIncome - item.trader.TradeList.Select(undefined, '2019-08').TotalAppreciation,
-  item.trader.TradeList.Select('2016-10', '2019-02').TotalIncome - item.trader.TradeList.Select('2020-04', '2021-08').TotalAppreciation,
+  item.trader.TradeList.Select('2017-02', '2019-02').TotalIncome - item.trader.TradeList.Select('2017-02', '2019-02').TotalAppreciation,
+  item.trader.TradeList.Select('2020-04', '2021-08').TotalIncome - item.trader.TradeList.Select('2020-04', '2021-08').TotalAppreciation,
+  item.trader.TradeList.Length,
 ]).map((item) => [
   item[0],
   item[1],
   item[2],
   nums([item[3], item[4]] as number[]).standardDeviation(),
-]));
+  item[5],
+]);
+
+fs.writeFileSync('1.json', JSON.stringify(a, null, 2));
 
 // const dayPrice = nums(dayData.map((item) => item.price));
 // const strategy = new Cross2LineReady(dayPrice.MA(8), dayPrice.MA(44));

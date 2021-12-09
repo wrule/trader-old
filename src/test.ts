56271b300e87fa3c2a02a0c5c6a783e4f4ffc9ab
jimao <jimao@jimaodeMacBook-Pro.local>
2021-12-09 11:08:03 +0800

    fix: 提交

--- src/test.ts
+++ src/test.ts
@@ -7,26 +7,25 @@ import moment from 'moment';
 import { MACross2LineReadyFinder } from './finder/MACross2LineReadyFinder';
 
 const dayData = loadFromCoinmarketcapData(data);
-// const finder = new MACross2LineReadyFinder([dayData], 1, 120);
-// const result = finder.Find();
-// console.log(result.size);
-// const list = Array.from(result.entries()).map(([key, value]) => {
-//   const trader = value[0].trader;
-//   return { key, trader };
-// });
-// list.sort((a, b) => b.trader.TradeList.TotalIncome - a.trader.TradeList.TotalIncome);
-// console.log(list.slice(0, 100).map((item) => [
-//   item.key,
-//   item.trader.TradeList.TotalIncome,
-//   item.trader.TradeList.ProfitRate,
-//   item.trader.TradeList.Select('2017-11', '2018-08').TotalIncome - item.trader.TradeList.Select('2017-11', '2018-08').TotalAppreciation,
-//   item.trader.TradeList.Select('2021-01', '2021-08').TotalIncome - item.trader.TradeList.Select('2021-01', '2021-08').TotalAppreciation,
-// ]));
+const finder = new MACross2LineReadyFinder([dayData], 1, 120);
+const result = finder.Find();
+console.log(result.size);
+const list = Array.from(result.entries()).map(([key, value]) => {
+  const trader = value[0].trader;
+  return { key, trader };
+});
+list.sort((a, b) => b.trader.TradeList.TotalIncome - a.trader.TradeList.TotalIncome);
+console.log(list.slice(0, 100).map((item) => [
+  item.key,
+  item.trader.TradeList.TotalIncome,
+  item.trader.TradeList.ProfitRate,
+  item.trader.TradeList.Select('2013-08', '2015-05').TotalIncome - item.trader.TradeList.Select('2013-08', '2015-05').TotalAppreciation,
+  item.trader.TradeList.Select('2016-10', '2019-02').TotalIncome - item.trader.TradeList.Select('2016-10', '2019-02').TotalAppreciation,
+  item.trader.TradeList.Select('2020-04', '2021-07').TotalIncome - item.trader.TradeList.Select('2020-04', '2021-07').TotalAppreciation,
+]));
 
-const dayPrice = nums(dayData.map((item) => item.price));
-const strategy = new Cross2LineReady(dayPrice.MA(8), dayPrice.MA(44));
-const trader = new Trader(strategy);
-trader.Backtesting(dayData);
-// trader.TradeList.Select('2013-08', '2015-04').PrintReport();
-trader.TradeList.Select('2017-03', '2018-12').PrintReport();
-// trader.TradeList.Select('2020-04').PrintReport();
+// const dayPrice = nums(dayData.map((item) => item.price));
+// const strategy = new Cross2LineReady(dayPrice.MA(8), dayPrice.MA(44));
+// const trader = new Trader(strategy);
+// trader.Backtesting(dayData);
+// trader.TradeList.Select('2017-03', '2018-12').PrintReport();
